218f4a8353a89805311d33d20e11645b50d6f222
Shaine Tsou <shaineagro06@gmail.com>
2021-02-01 22:23:38 +0800

    fix looping problem of useEffect hook

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,12 +5,21 @@ import Scroll from '../components/Scroll';
 import './App.css';
 
 function App() {
+    /* 
+    When the App function runs, it creates the initial state, after the first render it runs useEffect.
+    The userEffect will update the robots state, which is another render, so userEffect gets run again, causing a continuous loop.
+    Ideally, the useEffect hook with a fetch method should only run once.
+    */
+    /* 
+    To avoid useEffect runs over and over again, add an empty array instead of any state variable as the second argument.
+    After the component mounts, the empyt array will no longer change, so the useEffect hook will only run initially.
+    */
     const [robots, setRobots] = useState([]);
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
         .then(users => setRobots(users));
-    })
+    }, []) 
     
     const [searchfield, setSearchfield] = useState('')
     const onSearchChange = (event) => {
